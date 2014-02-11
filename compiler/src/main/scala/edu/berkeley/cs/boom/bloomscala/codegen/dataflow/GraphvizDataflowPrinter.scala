e687fc9deb2c2c77e505efacc39ac52e8d1d6468
Josh Rosen <rosenville@gmail.com>
2014-02-10 18:38:58 -0800

    Modify the GraphViz to show table scanners.

--- compiler/src/main/scala/edu/berkeley/cs/boom/bloomscala/codegen/dataflow/GraphvizDataflowPrinter.scala
+++ compiler/src/main/scala/edu/berkeley/cs/boom/bloomscala/codegen/dataflow/GraphvizDataflowPrinter.scala
@@ -2,30 +2,29 @@ package edu.berkeley.cs.boom.bloomscala.codegen.dataflow
 
 import scala.collection.mutable
 import edu.berkeley.cs.boom.bloomscala.analysis.Stratum
+import edu.berkeley.cs.boom.bloomscala.util.GraphvizPrettyPrinter
 
 
 /**
- * Translates a dataflow graph to a .dot file that can be
- * viewed using GraphViz.
- *
- * To make the graph easier to read, tables are displayed twice as
- * separate source and sink nodes.
+ * Translates a dataflow graph to a .dot file that can be viewed using GraphViz.
  */
-object GraphvizDataflowPrinter extends DataflowCodeGenerator {
+object GraphvizDataflowPrinter extends DataflowCodeGenerator with GraphvizPrettyPrinter {
 
-  private def label(elem: DataflowElementBase): String = {
+  private def label(elem: DataflowElement): String = {
     elem match {
       case Table(collection) =>
         collection.name
-      case e: DataflowElementBase =>
+      case Scanner(table) =>
+        table.collection.name + " Scanner"
+      case e: DataflowElement =>
         e.getClass.getSimpleName
     }
   }
 
-  private def shape(elem: DataflowElementBase): String = {
+  private def shape(elem: DataflowElement): String = {
     elem match {
       case t: Table => "rectangle"
-      case e: DataflowElementBase => "ellipse"
+      case e: DataflowElement => "ellipse"
     }
   }
 
@@ -43,17 +42,11 @@ object GraphvizDataflowPrinter extends DataflowCodeGenerator {
       val stratumStatements = mutable.Buffer[Doc]()
       clusteredStatements(stratum) = stratumStatements
 
-      val tables = elements.filter(_.isInstanceOf[Table]).map(_.asInstanceOf[Table])
-      val nonTables = elements.filterNot(_.isInstanceOf[Table])
-
       def processPort(outPort: OutputPort) {
         outPort.connectedPorts.foreach { inPort =>
           val edgeCrossesStratum = inPort.elem.stratum != stratum
-          val sink = inPort.elem match {
-            case t: Table => "sink"
-            case _ => ""
-          }
-          val edge = text(s"""${outPort.elem.id} -> $sink${inPort.elem.id} [headlabel="${inPort.name}",taillabel="${outPort.name}",fontsize=8,arrowsize=0.5];""")
+          val edge = diEdge(outPort.elem.id, inPort.elem.id, "headlabel" -> inPort.name, "taillabel" -> outPort.name,
+            "fontsize" -> "8", "arrowsize" -> "0.5")
           if (edgeCrossesStratum) {
             topLevelStatements += edge
           } else {
@@ -62,28 +55,14 @@ object GraphvizDataflowPrinter extends DataflowCodeGenerator {
         }
       }
 
-      // To make the graph easier to read, non-constant tables are displayed
-      // as separate source and sink nodes:
-      tables.foreach { t =>
-        stratumStatements += text(s"""${t.id} [label="${label(t)}",shape="${shape(t)}"];""")
-        if (t.hasInputs) {
-          stratumStatements += text(s"""sink${t.id} [label="${label(t)}",shape="${shape(t)}"];""")
-        }
-        t.outputPorts.foreach(processPort)
-      }
-
-      nonTables.foreach { e =>
-        stratumStatements += text(s"""${e.id} [label="${label(e)}",shape="${shape(e)}"];""")
+      elements.foreach { e =>
+        stratumStatements += node(e.id, "label" -> label(e), "shape" -> shape(e))
         e.outputPorts.foreach(processPort)
       }
     }
 
     val clusterDot = clusteredStatements.map { case (stratum, dotStatements) =>
-      "subgraph" <+> s"cluster${stratum.underlying}" <+> braces(nest(
-        linebreak <>
-        s"""label="Stratum ${stratum.underlying}";""" <@@>
-        dotStatements.reduce(_ <@@> _)
-      ) <> linebreak)
+      subgraph(s"cluster${stratum.underlying}", s"Stratum ${stratum.underlying}", dotStatements)
     }
 
     val dot = "digraph" <+> "dataflow" <+> braces(nest(
