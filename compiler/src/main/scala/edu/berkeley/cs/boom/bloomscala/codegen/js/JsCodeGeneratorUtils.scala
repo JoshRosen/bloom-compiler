14a39c67f1bd4ff9b3a4e61dad995cb3672ef642
Josh Rosen <rosenville@gmail.com>
2014-02-20 11:36:16 -0800

    Add comments to generated JS to show Bloom exprs.

--- compiler/src/main/scala/edu/berkeley/cs/boom/bloomscala/codegen/js/JsCodeGeneratorUtils.scala
+++ compiler/src/main/scala/edu/berkeley/cs/boom/bloomscala/codegen/js/JsCodeGeneratorUtils.scala
@@ -5,6 +5,7 @@ import org.kiama.output.PrettyPrinter
 import edu.berkeley.cs.boom.bloomscala.ast.BoundFieldRef
 import edu.berkeley.cs.boom.bloomscala.ast.PlusStatement
 import scala.collection.immutable
+import edu.berkeley.cs.boom.bloomscala.parser.BloomPrettyPrinter
 
 /**
  * Mixin that provides utility functions for Javascript code generation.
@@ -23,12 +24,17 @@ trait JsCodeGeneratorUtils extends PrettyPrinter {
     functionName <> parens(group(nest(ssep(argsSeq, comma <> line))))
   }
 
+  def comment(doc: Doc): Doc = {
+    "/*" <+> doc <+> "*/"
+  }
+
   /**
    * Translate an expression into a Javascript lambda function.
    */
   def genLambda(expr: Expr, parameterNames: List[String], exprParameterNames: Option[List[String]] = None): Doc = {
     "function" <> parens(parameterNames.map(text).reduce(_ <> comma <+> _)) <+> braces {
-      space <> "return" <+> genExpr(expr, exprParameterNames.getOrElse(parameterNames)) <> semi <> space
+      space <> "return" <+> genExpr(expr, exprParameterNames.getOrElse(parameterNames)) <> semi <+>
+        comment(BloomPrettyPrinter.pretty(expr)) <> space
     }
   }
 
