39cf4d260e8577625aca2833b8c3563a4a7ce094
Josh Rosen <rosenville@gmail.com>
2014-04-03 11:33:46 -0700

    Remove EndOfIteration for now, since it's unused.
    We might need to re-introduce similar flush-triggering
    punctuations if we generate dataflows that contain blocking
    operators.

--- rxflow/src/punctuations.js
+++ rxflow/src/punctuations.js
@@ -26,28 +26,12 @@ var EndOfRound = (function (_super) {
 exports.EndOfRound = EndOfRound;
 
 /**
-* Punctuation generated in cyclic dataflows in order to flush buffers so that we keep
-* making progress.  In a parallel implementation, we might use this as part of a
-* termination-detection algorithm, but that's unnecessary here.
-*/
-var EndOfIteration = (function (_super) {
-    __extends(EndOfIteration, _super);
-    function EndOfIteration(iteration) {
-        _super.call(this);
-        this.iteration = iteration;
-    }
-    return EndOfIteration;
-})(Punctuation);
-exports.EndOfIteration = EndOfIteration;
-
-/**
 * Mixin trait that implements the state machine governing punctuation propagation.
 */
 var PunctuationHandlerMixin = (function () {
     function PunctuationHandlerMixin() {
         this.currentRound = 0;
         this.eorCount = 0;
-        this.eoiCount = 0;
     }
     PunctuationHandlerMixin.prototype.getNumInputs = function () {
         throw new Error('getNumInputs() is abstract and must be implemented by subclasses');
@@ -67,22 +51,14 @@ var PunctuationHandlerMixin = (function () {
 
     PunctuationHandlerMixin.prototype.handlePunctuation = function (punc, source) {
         var numInputs = this.getNumInputs();
-        if (punc instanceof EndOfIteration) {
-            this.eoiCount += 1;
-        } else if (punc instanceof EndOfRound && punc.round === this.currentRound) {
+        if (punc instanceof EndOfRound && punc.round === this.currentRound) {
             this.eorCount += 1;
             if (this.eorCount === numInputs || numInputs === 0) {
                 this.eorCount = 0;
-                this.eoiCount = 0;
                 this.currentRound += 1;
                 this.handleEndOfRound();
                 this.sendPunctuationDownstream(punc);
-                return;
             }
-        }
-        if (this.eoiCount === (numInputs - this.eorCount) || numInputs === 0) {
-            this.eoiCount = 0;
-            this.flush();
         }
     };
     return PunctuationHandlerMixin;
--- rxflow/src/punctuations.ts
+++ rxflow/src/punctuations.ts
@@ -16,25 +16,11 @@ export class EndOfRound extends Punctuation {
 }
 
 /**
- * Punctuation generated in cyclic dataflows in order to flush buffers so that we keep
- * making progress.  In a parallel implementation, we might use this as part of a
- * termination-detection algorithm, but that's unnecessary here.
- */
-export class EndOfIteration extends Punctuation {
-    iteration: number;
-    constructor(iteration: number) {
-        super();
-        this.iteration = iteration;
-    }
-}
-
-/**
  * Mixin trait that implements the state machine governing punctuation propagation.
  */
 export class PunctuationHandlerMixin {
     private currentRound = 0;
     private eorCount = 0;
-    private eoiCount = 0;
 
     getNumInputs(): number {
         throw new Error('getNumInputs() is abstract and must be implemented by subclasses');
@@ -54,22 +40,14 @@ export class PunctuationHandlerMixin {
 
     handlePunctuation(punc: Punctuation, source: any): void {
         var numInputs = this.getNumInputs();
-        if (punc instanceof EndOfIteration) {
-            this.eoiCount += 1;
-        } else if (punc instanceof EndOfRound && (<EndOfRound> punc).round === this.currentRound) {
+        if (punc instanceof EndOfRound && (<EndOfRound> punc).round === this.currentRound) {
             this.eorCount += 1;
             if (this.eorCount === numInputs || numInputs === 0) {
                 this.eorCount = 0;
-                this.eoiCount = 0;
                 this.currentRound += 1;
                 this.handleEndOfRound();
                 this.sendPunctuationDownstream(punc);
-                return;
             }
-        }
-        if (this.eoiCount === (numInputs - this.eorCount) || numInputs === 0) {
-            this.eoiCount = 0;
-            this.flush();
         }
     }
 }
